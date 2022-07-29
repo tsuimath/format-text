ce2578628dee26283d7f9dc7cc7f176cffcc94da
Matheus Tsutsui <matheus.tsutsui@webjump.com.br>
2022-07-29 01:26:03 -0300

    Add files

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function AppViewModel() {
+    this.displayView = ko.observable('')
+    this.toUpperCase = function() {
+        let currentText = this.displayView()
+        this.displayView(currentText.toUpperCase())
+    }
+    this.toLowerCase = function() {
+        let currentText = this.displayView()
+        this.displayView(currentText.toLowerCase())
+    }
+    this.capitalize = function() {
+        let currentText = this.displayView(),
+            letterCapitalized = currentText.charAt(0).toUpperCase(),
+            theRestOfText = currentText.slice(1)
+
+        this.displayView(letterCapitalized + theRestOfText)
+    }
+}
+
+
+ko.applyBindings(new AppViewModel())
